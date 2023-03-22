d2b3d51e699e0531f8ac2878da0e0824df7d03af
David Metzler <dm-213782@hswgt.hs-weingarten.de>
2023-03-22 16:39:28 +0100

    attemt 1 to use library

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Hello</h1>>
+    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+</template>
+  
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+    name: 'Home',
+    components: { StreamBarcodeReader },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        onDecode(result) { console.log(result) },
+        onLoaded(result) { console.log(result) }
+    }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
