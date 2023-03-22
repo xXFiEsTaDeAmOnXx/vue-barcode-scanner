01c52b027f09e16c34a12788af77d96128853037
M. Dogan <dogan-munzur@outlook.de>
2023-03-22 18:25:33 +0100

    Barcode Success Message, CSS

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,7 +1,11 @@
 <template>
-    <h1>Hello</h1>
+    <h1>QR & BARCODE SCANNER</h1>
     <p>{{result}}</p>
-    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+    <StreamBarcodeReader id = "video" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+    <div id="app">
+    <p v-if="showSuccessMessage">Barcode erfolgreich gescannt!</p>
+</div>
+
 </template>
   
 <script>
@@ -11,16 +15,39 @@ export default {
     name: 'homePage',
     components: { StreamBarcodeReader },
     data() {
+        
         return {
+            showSuccessMessage: false,
                 result: ""
+                
         }
     },
     methods: {
-        onDecode(result) { this.result = result },
+       
+        onDecode(result) { this.result = result;  showSuccessMessage: true},
         onLoaded(result) { console.log(result) }
+        
     }
 }
 </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style 
+scoped>
+
+h1 {
+    background-color: rgba(72, 206, 197, 0.319);
+    text-align: center;
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 24pt;
+    color: rgb(93, 93, 93);
+}
+
+#video {
+
+    width: fit-content;
+    
+    
+}
+
+</style>
